5cb710378092c0ff12748d701e2a3a892f08042f
Vishal ArorA <48501332+vishal-op-arora@users.noreply.github.com>
2022-05-23 23:01:38 +0530

    Add files via upload

--- Lab 04/Question9.sql
+++ Lab 04/Question9.sql
@@ -1,3 +1,6 @@
+-- Question 9 : Create a stored procedure to display supplier id, name, rating and Type_of_Service. For Type_of_Service, If rating =5, print “Excellent 
+-- Service”,If rating >4 print “Good Service”, If rating >2 print “Average Service” else print “Poor Service”
+
 drop procedure if exists supplierRatings ;
 DELIMITER &&  
 CREATE PROCEDURE supplierRatings ()  
